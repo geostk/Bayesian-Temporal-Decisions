efeeaa07c3fdf399d91fdefbb778e912445af51b
James Carroll <jamescarroll@protonmail.ch>
2017-04-10 15:25:13 -0400

    add TM and OM for filtering problem

--- filtrieren.py
+++ filtrieren.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 terrain = np.matrix(
 [['H', 'N', 'T'],
@@ -8,16 +9,58 @@ terrain = np.matrix(
 prior = np.matrix(
 [[0.125, 0.125, 0.125],
 [0.125, 0.125, 0.125], 
-[0.125, 0.125, 0.125]])
+[0.125, 0, 0.125]]) #prior[2,1] == 'B'
 
-actions = ['R', 'R', 'R', 'R']
+actions = ['Right', 'Right', 'Down', 'Down']
 readings = ['N', 'N', 'H', 'H']
 
-def move(prevmap, action):
-	for pos in np.nditer(prevmap):
-		
+def TransitionModel(cellfromimmutable, action):
+	cellfrom = list(cellfromimmutable)
+	cellto = cellfrom #cellto changes when cellfrom changes. bc python
+	if action == 'Right':
+		cellto[0] += 1
+	elif action == 'Left':
+		cellto[0] -= 1
+	elif action == 'Up':
+		cellto[1] += 1
+	elif action == 'Down':
+		cellto[1] -= 1
+	else:
+		return nil
 
-	return none
+	if cellto[0] <= -1 or cellto[0] >= 3:
+		return list(cellfromimmutable)
+	elif cellto[1] <= -1 or cellto[1] >= 3:
+		return list(cellfromimmutable)
+	elif celllto[0] == 1 and cellto[1] == 2:
+		return list(cellfromimmutable)
+	
+	if random.randrange(0, 100) < 90:
+		return cellto
+	else:
+		return list(cellfromimmutable) 
 
-for x in np.nditer(prior):
-	print x
+def ObservationModel(reading):
+	prob = random.randrange(0, 100)
+	if prob < 90:
+		return reading
+	elif prob >= 90 and prob < 95:
+		if reading == 'N':
+			return 'H'
+		elif reading == 'H':
+			return 'T'
+		elif reading == 'T':
+			return 'N'
+	elif prob >= 95 and prob < 100:
+		if reading == 'N':
+			return 'T'
+		elif reading == 'H':
+			return 'N'
+		elif reading == 'T':
+			return 'H'			
+	
+#for x in np.nditer(prior):
+#	print x
+
+print TransitionModel((2,0), 'Right') #cannot do so, other is blocked
+print ObservationModel(terrain[2,0])
