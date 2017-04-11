32cd18117d0475f47673672d45d616c5129e0fd7
James Carroll <jamescarroll@protonmail.ch>
2017-04-11 16:39:55 -0400

    it generates the first line at least

--- generatemaps.py
+++ generatemaps.py
@@ -2,16 +2,30 @@ import numpy as np
 import random
 from filtrieren import TransitionModel, ObservationModel
 
-#TODO: import TM and OM from filtrieren.py
-
-maps = ['N']*40 + ['H']*20 + ['T']*20 + ['B']*10
-random.shuffle(maps)
+terrains = ['N']*40 + ['H']*20 + ['T']*20 + ['B']*10
+random.shuffle(terrains)
+GrossesGelaende = np.asmatrix(terrains)
+GrossesGelaende = np.reshape(GrossesGelaende, (9, 10))
 
 def startingplace():
-	index = random.randrange(0, 99)
-	if maps[index] == 'B':
+	indX = random.randrange(0,9)
+	indY = random.randrange(0,9)
+
+	if GrossesGelaende[indX,indY] == 'B':
 		return startingplace()
 	else:
-		return maps[index]
+		return [indX,indY]
 
-print startingplace()
+def generatefile():
+	firstline = "x0y0: " + str(startingplace()) + "\n"
+
+	target = open("output.txt", "w")
+	target.write(firstline)
+	target.close()
+
+def main():
+	print GrossesGelaende
+	generatefile()
+
+if __name__ == "__main__":
+	main()
