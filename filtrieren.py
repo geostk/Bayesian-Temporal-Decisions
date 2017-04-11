250e7c6905fd332dff453d6c1466f87433fa0daf
James Carroll <jamescarroll@protonmail.ch>
2017-04-11 18:22:42 -0400

    still buggy, TM won't work, but showing outline

--- filtrieren.py
+++ filtrieren.py
@@ -17,13 +17,13 @@ readings = ['N', 'N', 'H', 'H']
 def TransitionModel(cellfromimmutable, action):
 	cellfrom = list(cellfromimmutable)
 	cellto = cellfrom #cellto changes when cellfrom changes. bc python
-	if action == 'Right':
+	if action == 'Right' or action == 'R':
 		cellto[0] += 1
-	elif action == 'Left':
+	elif action == 'Left' or action == 'L':
 		cellto[0] -= 1
-	elif action == 'Up':
+	elif action == 'Up' or action == 'U':
 		cellto[1] += 1
-	elif action == 'Down':
+	elif action == 'Down' or action == 'D':
 		cellto[1] -= 1
 	else:
 		return nil
@@ -32,7 +32,7 @@ def TransitionModel(cellfromimmutable, action):
 		return list(cellfromimmutable)
 	elif cellto[1] <= -1 or cellto[1] >= 3:
 		return list(cellfromimmutable)
-	elif celllto[0] == 1 and cellto[1] == 2:
+	elif cellto[0] == 1 and cellto[1] == 2:
 		return list(cellfromimmutable)
 	
 	if random.randrange(0, 100) < 90:
--- generatemaps.py
+++ generatemaps.py
@@ -1,11 +1,11 @@
 import numpy as np
+import itertools
 import random
 from filtrieren import TransitionModel, ObservationModel
 
 terrains = ['N']*40 + ['H']*20 + ['T']*20 + ['B']*10
 random.shuffle(terrains)
-GrossesGelaende = np.asmatrix(terrains)
-GrossesGelaende = np.reshape(GrossesGelaende, (9, 10))
+GrossesGelaende = np.reshape(np.asmatrix(terrains), (9, 10))
 
 def startingplace():
 	indX = random.randrange(0,9)
@@ -14,18 +14,48 @@ def startingplace():
 	if GrossesGelaende[indX,indY] == 'B':
 		return startingplace()
 	else:
-		return [indX,indY]
+		return (indX,indY)
 
-def generatefile():
-	firstline = "x0y0: " + str(startingplace()) + "\n"
+def randomactions():
+	actionseq = []
+	possibleactions = ['U', 'D', 'L', 'R']
 
-	target = open("output.txt", "w")
-	target.write(firstline)
+	for _ in itertools.repeat(None, 100):
+		actionseq.append(possibleactions[random.randrange(0,3)])
+
+	return actionseq
+
+def PointsAndReadings(start, actionseq):
+	points = [] 
+	readings = []
+
+	cellfrom = start
+	for i in range(0, 100):
+		latest = tuple(TransitionModel(cellfrom, actionseq[i]))
+		print i
+		points.append(latest)
+		readings.append(ObservationModel(GrossesGelaende[latest]))
+		cellfrom = latest
+
+	return points,readings
+
+def generatefile(index):
+	sp = startingplace()
+	actionseq = randomactions()
+	points, readings = PointsAndReadings(sp, actionseq)
+	
+	target = open("output" + str(index) + ".txt", "w")
+
+	target.write(str(GrossesGelaende) + "\n")
+	target.write("x_0y_0: " + str(sp) + "\n")
+	target.write("x_iy_i: " + str(points) + "\n")
+	target.write("\\alpha_i: " + str(actionseq) + "\n")
+	target.write("\\epsilon_i: " + str(readings))
+
 	target.close()
 
 def main():
-	print GrossesGelaende
-	generatefile()
+	generatefile(0)
 
 if __name__ == "__main__":
 	main()
