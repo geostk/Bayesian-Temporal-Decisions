c1d9397d2241ffc309a62086cdb001871b4f4fdc
James Carroll <jamescarroll@protonmail.ch>
2017-04-11 22:37:57 -0400

    theoretical framework for 5a

--- filtrieren.py
+++ filtrieren.py
@@ -11,8 +11,8 @@ prior = np.matrix(
 [0.125, 0.125, 0.125], 
 [0.125, 0, 0.125]]) #prior[2,1] == 'B'
 
-actions = ['R', 'R', 'D', 'D']
-readings = ['N', 'N', 'H', 'H']
+given_actions = ['R', 'R', 'D', 'D']
+given_readings = ['N', 'N', 'H', 'H']
 
 def TransitionModel(mappo, cellfromimmutable, action):
 	cellfrom = list(cellfromimmutable)
@@ -57,11 +57,30 @@ def ObservationModel(reading):
 		elif reading == 'H':
 			return 'N'
 		elif reading == 'T':
-			return 'H'			
-	
-#for x in np.nditer(prior):
-#	print x
+			return 'H'
+			
+def Filter(mappo, action, reading):
+
+	for x in range(0, np.shape(mappo)[0]):
+		for y in range(0, np.shape(mappo)[1]):
+			priorbelief = mappo[(x,y)]
+			#multiply priorbelief by TM and OM
+
+	#for stateprob in np.nditer(mappo):
+	#	print stateprob
+
+	return mappo
 
 def main():
-	print TransitionModel((2,0), 'Right') #cannot do so, other is blocked
-	print ObservationModel(terrain[2,0])
+	map1 = Filter(prior, given_actions[0], given_readings[0])
+	#map2 = Filter(map1, given_actions[1], given_readings[1])
+	#map3 = Filter(map2, given_actions[2], given_readings[2])
+	#map4 = Filter(map3, given_actions[3], given_readings[3])
+
+	print map1
+
+#print TransitionModel((2,0), 'Right') #cannot do so, other is blocked
+#print ObservationModel(terrain[2,0])
+
+if __name__ == "__main__":
+	main()
